d172094ed87f3e3b23cd46c4700173b29748c25a
Maria Alejandra Diaz <83391723+mariaadiaz@users.noreply.github.com>
2026-05-21 01:10:46 +0200

    Delete Energy_cost_from_EMG  directory

--- Energy_cost_from_EMG /DelsysPythonDemo.py
+++
@@ -1,9 +0,0 @@
-from UIControls.FrameController import *
-
-def main():
-    app = QApplication(sys.argv)
-    controller = FrameController()
-    sys.exit(app.exec_())
-
-if __name__ == '__main__':
-    main()
